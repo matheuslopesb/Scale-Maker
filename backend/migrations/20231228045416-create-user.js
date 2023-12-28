722f0490b652e6a38fae0adcca5db63b60775196
Matheus Lopes Brito <teteu260505@gmail.com>
2023-12-28 02:15:27 -0300

    Criando e organizando diretórios e arquivos, criando e conectando-se com o banco de dados e criando a migration e tabela "User"

---
+++ backend/migrations/20231228045416-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(100),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(20),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+};
---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+// src/index.ts
+import express from 'express';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+})
+
+pool.query('SELECT NOW()', (err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados', err);
+    } else {
+        console.log(`Conexão bem-sucedida ao banco de dados`);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
